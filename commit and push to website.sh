9db3dc3e2a4a66d17dbc063456aeee9baed24a20
Omar Barbiero <omar.barbiero@gmail.com>
2020-10-19 16:29:51 -0400

    Fixed typo on automatic push

--- commit and push to website.sh
+++ commit and push to website.sh
@@ -3,7 +3,6 @@ cd "C:\Users\Omar\Dropbox\CV"
 git add .
 git commit -m "updated CV content"
 git push origin master
-git push overleaf master
 cp -r CV.pdf "C:\Users\Omar\jekyll-projects\website\files\CV.pdf"
 cd "C:\Users\Omar\jekyll-projects\website"
 git add .
